bd1a3e00ba393dabc3b1abc2e4eb242a73087ab6
abbagana <31752841+abbagana@users.noreply.github.com>
2020-05-20 10:53:31 +0100

    Add files via upload

---
+++ @@cht/index.php
@@ -0,0 +1,139 @@
+<?php
+session_start ();
+function loginForm() {
+    echo '
+	<div class="form-group">
+	<th class="nav" align="center"><img src="images/logo.jpg" width="200" height="150"></th>
+	<strong><marquee behavior="alternate">WELCOME TO COMPUTER SCIENCE DEPARTMENT F.C.E - YOLA CHAT SITE</marquee></span></font></div></strong>
+	</br></br><a href="../choose.php">Goto Home Page</a>
+		<div id="loginform">
+			<form action="index.php" method="post">
+			<h1>Live Chat</h1><hr/>
+				<label for="name">Please Enter Your Name To Continue</label>
+				<input type="text" name="name" id="name" class="form-control" placeholder="Enter Your Name"/>
+				<input type="submit" class="btn btn-default" name="enter" id="enter" value="Enter" />
+			</form>
+		</div>
+	</div>
+   ';
+}
+ 
+if (isset ( $_POST ['enter'] )) {
+    if ($_POST ['name'] != "") {
+        $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
+        $cb = fopen ( "log.html", 'a' );
+        fwrite ( $cb, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat.</i><br></div>" );
+        fclose ( $cb );
+    } else {
+        echo '<span class="error">Please Enter a Name</span>';
+    }
+}
+ 
+if (isset ( $_GET ['logout'] )) {
+    $cb = fopen ( "log.html", 'a' );
+    fwrite ( $cb, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has left the chat.</i><br></div>" );
+    fclose ( $cb );
+    session_destroy ();
+    header ( "Location: index.php" );
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NACOSS Live Chat </title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<!--
+		*******************************************************************************
+
+		I M P O R T A N T -- N O T I C E
+		- - - - - - - - -    - - - - - -
+
+		created by MUhammad Abba Gana
+
+		Do NOT copy and paste this code from a web browser "View Source" Window.
+
+		The actual code contains server-side function calls which do not appear in a
+		"View Source" situation even copy cannot work!!
+
+		Please refer to the supplied templates on the product CD.
+		
+			for more contact me on the following details;
+
+			email address: abbagana79@gmail.com
+			phone number: +2349039016969
+			website:www.Guidetricks.blogspot.com
+
+		*******************************************************************************
+	-->
+<body>
+<?php
+	if (! isset ( $_SESSION ['name'] )) {
+	loginForm ();
+	} else {
+?>
+<div id="wrapper">
+	<div id="menu">
+	<h1>Live Chat!</h1><hr/>
+		<p class="welcome"><b>HI - <a><?php echo $_SESSION['name']; ?></a></b></p>
+		<p class="logout"><a id="exit" href="#" class="btn btn-default">Exit Live Chat</a></p>
+	<div style="clear: both"></div>
+	</div>
+	<div id="chatbox">
+	<?php
+		if (file_exists ( "log.html" ) && filesize ( "log.html" ) > 0) {
+		$handle = fopen ( "log.html", "r" );
+		$contents = fread ( $handle, filesize ( "log.html" ) );
+		fclose ( $handle );
+
+		echo $contents;
+		}
+	?>
+	</div>
+<form name="message" action="">
+	<input name="usermsg" class="form-control" type="text" id="usermsg" placeholder="Create A Message" />
+	<input name="submitmsg" class="btn btn-default" type="submit" id="submitmsg" value="Send" />
+</form>
+</div>
+<script type="text/javascript">
+$(document).ready(function(){
+});
+$(document).ready(function(){
+    $("#exit").click(function(){
+        var exit = confirm("Are You Sure You Want To Leave This Page?");
+        if(exit==true){window.location = 'index.php?logout=true';}     
+    });
+});
+$("#submitmsg").click(function(){
+        var clientmsg = $("#usermsg").val();
+        $.post("post.php", {text: clientmsg});             
+        $("#usermsg").attr("value", "");
+        loadLog;
+    return false;
+});
+function loadLog(){    
+    var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+    $.ajax({
+        url: "log.html",
+        cache: false,
+        success: function(html){       
+            $("#chatbox").html(html);       
+            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+            if(newscrollHeight > oldscrollHeight){
+                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal');
+            }              
+        },
+    });
+}
+setInterval (loadLog, 2500);
+</script>
+<?php
+}
+?>
+</body>
+<p><Strong><center> Programmed by Muhammad Abba Gana</center></Strong></p>
+<p><Strong><center> Design by Habibullah .Y. Usman</center></Strong></p>
+<p><Strong><center> Analyse by Abubakar Umar Bello</center></Strong></p>
+</html>
